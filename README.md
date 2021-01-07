c489028c87202eb7a1edb8d0529802dab408d22a
meokisama <hoangdinhsang123@gmail.com>
2021-01-07 14:57:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+<p align="center">
+  <a href="https://meokisama.github.io">
+    <img src="favicon.png" />
+  </a>
+</p>
+
+<h1 align="center"> VSCode Context Menu For Nautilus </h1>
+
+
+<p align="center">
+  <a href="https://github.com/meokisama/meokisama.github.io/blob/develop/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg"/>
+  </a>
+  <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg"/>
+  <a href="https://twitter.com/intent/follow?screen_name=meokiiii">
+    <img src="https://img.shields.io/twitter/follow/meokiiii.svg?label=Follow%20@meokiiii"/>
+  </a>
+</p>
+
+
+A small repository provides a Visual Studio Code extension to context menu for Nautilus.
+
+### Install Extension
+
+```
+wget -qO- https://raw.githubusercontent.com/meokisama/vscodemenu/master/install.sh | bash
+```
+
+### Uninstall Extension
+
+```
+rm -f ~/.local/share/nautilus-python/extensions/code-nautilus.py
+```
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Install python3-nautilus
+echo "Installing python3-nautilus...\n"
+if type "pacman" > /dev/null 2>&1
+then
+    sudo pacman -S --noconfirm python3-nautilus
+elif type "apt-get" > /dev/null 2>&1
+then
+    installed=`apt list --installed python3-nautilus -qq 2> /dev/null`
+    if [ -z "$installed" ]
+    then
+        sudo apt-get install -y python3-nautilus
+    else
+        echo "python3-nautilus is already installed.\n"
+    fi
+elif type "dnf" > /dev/null 2>&1
+then
+    installed=`dnf list --installed nautilus-python 2> /dev/null`
+    if [ -z "$installed" ]
+    then
+        sudo dnf install -y nautilus-python
+    else
+        echo "nautilus-python is already installed.\n"
+    fi
+else
+    echo "Failed to find python3-nautilus, please install it manually.\n"
+fi
+
+# Remove previous version and setup folder
+echo "Removing previous version (if found)..."
+mkdir -p ~/.local/share/nautilus-python/extensions
+rm -f ~/.local/share/nautilus-python/extensions/VSCodeExtension.py
+rm -f ~/.local/share/nautilus-python/extensions/code-nautilus.py
+rm -f ~/.local/share/nautilus-python/extensions/vscodemenu.py
+
+# Download and install the extension
+echo "Downloading newest version...\n"
+wget --show-progress -q -O ~/.local/share/nautilus-python/extensions/vscodemenu.py https://raw.githubusercontent.com/meokisama/vscodemenu/master/vscodemenu.py
+
+# Restart nautilus
+echo "Restarting nautilus...\n"
+nautilus -q
+
+echo "Installation Complete\n"
